fd18dcc8949b864ce68e728ebce4177d0c4d9667
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-06-11 23:56:31 +0100

    Add movement keys

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -13,16 +13,43 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
-        const tiles = this.add.image(400, 300, 'tiles');
-        const logo = this.add.image(400, 150, 'logo');
+        this.tiles = this.add.image(400, 300, 'tiles');
+        this.logo = this.add.image(400, 150, 'logo');
         
         this.tweens.add({
-          targets: logo,
+          targets: this.logo,
           y: 450,
           duration: 1000,
           ease: "Power2",
           yoyo: true,
           loop: -1
         });
+
+        this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
+        this.input.on('pointerdown', (e) => {
+            this.tiles.x = e.x;
+            this.tiles.y = e.y;
+        }, this);
+    }
+
+    update(delta) {
+        if (this.keyLeft.isDown) {
+            this.tiles.x--;
+        }
+
+        if (this.keyRight.isDown) {
+            this.tiles.x++;
+        }
+        if (this.keyUp.isDown) {
+            this.tiles.y--;
+        }
+
+        if (this.keyDown.isDown) {
+            this.tiles.y++;
+        }
     }
 }
