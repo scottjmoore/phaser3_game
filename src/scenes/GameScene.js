c978bb02c93fbec5fe3b3df70f0568954171b1cd
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-06-11 23:38:10 +0100

    Initial commit

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,25 @@
+import 'phaser';
+import logoImg from '../assets/logo.png';
+
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super('Game');
+    }
+
+    preload() {
+        this.load.image('logo', logoImg);
+    }
+
+    create() {
+        const logo = this.add.image(400, 150, 'logo');
+  
+        this.tweens.add({
+          targets: logo,
+          y: 450,
+          duration: 1000,
+          ease: "Power2",
+          yoyo: true,
+          loop: -1
+        });
+    }
+}
