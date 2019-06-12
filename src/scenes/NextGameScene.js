79f584040c50b00dad73d0396f9e5db5014b29bb
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-06-12 23:01:38 +0100

    Add mario style level

--- src/scenes/NextGameScene.js
+++ src/scenes/NextGameScene.js
@@ -1,6 +1,7 @@
 import 'phaser';
 import logoImg from '../assets/logo.png';
 import tilesImg from '../assets/classical_ruin_tiles.png';
+import marioTilesImg from '../assets/mario_tiles.png';
 
 export default class NextGameScene extends Phaser.Scene {
     constructor() {
@@ -12,14 +13,48 @@ export default class NextGameScene extends Phaser.Scene {
     preload() {
         this.load.image('logo', logoImg);
         this.load.image('newtiles', tilesImg);
+        this.load.image('mariotiles', marioTilesImg);
     }
 
     create() {
-        this.tiles = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'newtiles');
         this.logo = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'logo');
-        this.tiles.setScale(3, 3);
         this.text = this.add.text(0, 0, "Welcome to the next scene!", {font: "40px Impact"});
         this.text.setColor('red');
+
+        const levelFront = [
+            [  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1 ],
+            [ -1,   1,   2,   3,   -1,   -1,   -1,   1,   2,   3,   -1 ],
+            [  -1,   5,   6,   7,   -1,   -1,   -1,   5,   6,   7,   -1 ],
+            [ -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1 ],
+            [  -1,   -1,   -1,  14,  13,  14,   -1,   -1,   -1,   -1,   -1 ],
+            [ -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1 ],
+            [  -1,   -1,   -1,   -1,   11,   -1,   -1,   -1,   -1,   -1,   -1 ],
+            [ -1,   -1,  14,  14,  14,  14,  14,   -1,   -1,   -1,  15 ],
+            [  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,  15,  15 ],
+            [ 35,  36,  37,   -1,   -1,   -1,   -1,   -1,  15,  15,  15 ],
+            [ 39,  39,  39,  39,  39,  39,  39,  39,  39,  39,  39 ]
+        ];
+
+        const levelBack = [
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ],
+            [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0 ]
+        ];
+
+        const mapFront = this.make.tilemap({ data: levelFront, tileWidth: 16, tileHeight: 16 });
+        const mapBack = this.make.tilemap({ data: levelBack, tileWidth: 16, tileHeight: 16 });
+        const marioTilesFront = mapFront.addTilesetImage('mariotiles');
+        const marioTilesBack = mapBack.addTilesetImage('mariotiles');
+        const layerBack = mapBack.createStaticLayer(0, marioTilesBack, 0, 0).setScale(4, 4).setDepth(-1);
+        const layerFront = mapFront.createStaticLayer(0, marioTilesFront, 0, 0).setScale(4, 4).setDepth(1);
 
         this.tweens.add({
           targets: this.logo,
